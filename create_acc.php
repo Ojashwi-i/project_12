81205a2767a50e633a3e88faf626a33f5b75f323
Ojashwi-i <o91373015@gmail.com>
2025-02-20 19:21:45 +0545

    edit 1

---
+++ create_acc.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="create_acc.css">
+    <script src="https://kit.fontawesome.com/03afbd77fe.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+        <div class="rectangle1">
+                <div style="position: absolute; margin: 0px 30px">
+                    <h1>Create an Account</h1>
+                    <h5 style="margin-bottom: 30px;">Already have an account? <a href="sign_in.php">Sign In</a></h5>
+                    <form action="">
+                        <div style="display: flex;">
+                            <div>
+                                <label for="fname">First Name</label> <br>
+                                <input class="input1" type="text"> <br>
+                            </div>
+                            <div style="margin-left:27px;">
+                                <label for="lname">Last Name</label> <br>
+                                <input class="input1" type="text"> <br>
+                            </div>
+                        </div>
+                        <label for="Uname">Username</label> <br>
+                        <input class="input2" type="text"> <br>
+                        <label for="email">Email Address</label> <br>
+                        <input class="input2" type="text"> <br>
+                        <label for="password">Password</label> <br>
+                        <input class="input2" type="password"> <br>
+                        <input type="submit" class="button1">
+                    </form>
+                </div>
+        </div>
+
+</body>
+
+</html>
---
+++ sign_in.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sign_in.css">
+    <script src="https://kit.fontawesome.com/03afbd77fe.js" crossorigin="anonymous"></script>
+    <title>login</title>
+</head>
+
+<body>
+        <div class="rectangle1">
+                <div style="position: absolute; margin: 0px 30px">
+                    <h1>Sign In</h1>
+                    <h5 style="margin-bottom: 30px;">Don't have an account? <a href="create_acc.php">Create One</a></h5>
+                    <form method="post">
+                        <label for="email">Email Address</label> <br>
+                        <input type="text"> <br>
+                        <label for="password">Password</label> <br>
+                        <input type="password"> <br>
+                        <input type="submit" class="button1">
+                    </form>
+                </div>
+        </div>
+</body>
+
+</html>
